d0f1089b02d4b5d56b61c810405e354d1e87eff3
jonel <iamjonel7@gmail.com>
2025-11-06 22:52:54 +0800

    Removed old unnecessary codes and used Switch inside the handling of events instead of if statements.

--- src/core/GameEngine.cpp
+++ src/core/GameEngine.cpp
@@ -1,161 +1,123 @@
 #include "GameEngine.h"
-#include "SceneMenu.h"
-#include <SFML/Graphics.hpp>
+//#include "SceneMain.h"
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <map>
 
 GameEngine::GameEngine()
 {
-   std::cout << "Game engine running.\n";
-
-   m_window.create(sf::VideoMode(1280, 720), "Revivers");
-
-   std::cout << "Window created.\n";
-
-   init();
-
-   changeScene("menu", std::make_shared<SceneMenu>(this));
+  init();
+  changeScene("main", std::make_shared<dummyScene>(this));
 }
 
 void GameEngine::init()
 {
-
-
-   //grammr first
-   m_grammarManager = std::make_unique<GrammarManager>();
-
-   //lexicon depends on grammar for language specific paths
-   std::string currentLanguage = m_grammarManager->getCurrentLanguageName();
-
-   std::cout << "Native language set to " << currentLanguage << std::endl;
-
-   std::string lexiconPath = "data/lexicon/" + currentLanguage + "_lexicon.json";
-
-   //TODO: create file is does not exist
-   std::string playerDictPath = "data/lexicon/" + currentLanguage + "_dictionary.json";
-
-   m_lexiconManager = std::make_unique<LexiconManager>(lexiconPath, playerDictPath);
-
-   std::cout << "Lexicon and player dictionary are loaded.\n";
-}
-
-//wont be using this for now, but this needs to be updated
-//GameEngine::changeLanguage(const std::string &language)
-//{
-//   m_grammarManager->setLanguage(language);
-//
-//   init();
-//}
-
-GameEngine::GameEngine(int width, int height, const std::string& title)
-   : m_window(sf::VideoMode(width, height), title)
-{
-   changeScene("menu", std::make_shared<SceneMenu>(this));
+  std::cout << "Game engine running.\n";
+  m_window.create(sf::VideoMode(WIDTH, HEIGHT), "Revivers");
+  std::cout << "Window created.\n";
 }
 
 void GameEngine::update()
 {
-   if(auto scene = currentScene())
-   {
+  if(auto scene = currentScene())
+    {
       scene->update();
-   }
+    }
 }
 
 void GameEngine::sUserInput()
 {
-   sf::Event event;
-   auto scene = currentScene();
-   auto& actionMap = scene->getActionMap();
+  sf::Event event;
+  auto scene = currentScene();
+  auto& actionMap = scene->getActionMap();
+  auto& textInputManager = scene->getTextInputManager();
 
-   while(m_window.pollEvent(event))
-   {
-      if(event.type == sf::Event::Closed)
-      {
-         quit();
-      }
-
-      if(event.type == sf::Event::TextEntered)
+  while(m_window.pollEvent(event))
+    {
+      switch(event.type)
         {
-          textInputManager.handleTextEvent(event.text);
-          continue;
+        case sf::Event::Closed:
+          quit();
+          break;
+
+        case sf::Event::KeyPressed:
+        case sf::Event::KeyReleased:
+          {
+            if(actionMap.find(event.key.code) == actionMap.end())
+              continue;
+
+            const std::string actionType = (event.type == sf::Event::KeyPressed) ? "START" : "END";
+            auto actionName = actionMap.at(event.key.code);
+            scene->doAction(Action(actionName, actionType));
+            break;
+          }
+        case sf::Event::TextEntered:
+          if(event.text.unicode < 128)
+            {
+              textInputManager.addCharacter(static_cast<char>(event.text));
+            }
+          break;
+        default:
+          break;
         }
-
-      if(event.type == sf::Event::KeyPressed || event.type == sf::Event::KeyReleased)
-      {
-         if(actionMap.find(event.key.code) == actionMap.end())
-            continue;
-
-         const std::string actionType = (event.type == sf::Event::KeyPressed) ? "START": "END";
-         auto actionName = actionMap.at(event.key.code);
-         scene->doAction(Action(actionName, actionType));
-      }
-   }
+    }
 }
 
 void GameEngine::sRender()
 {
-   auto scene = currentScene();
-   if(scene)
-      scene->sRender();
+  auto scene = currentScene();
+  if(scene)
+    scene->sRender();
 }
 
 void GameEngine::changeScene(const std::string& sceneName, std::shared_ptr<Scene> scene, bool endCurrentScene)
 {
-   if(endCurrentScene && m_sceneMap.count(m_currentScene))
-   {
+  if(endCurrentScene && m_sceneMap.count(m_currentScene))
+    {
       m_sceneMap.erase(m_currentScene);
-   }
+    }
 
-   m_sceneMap[sceneName] = scene;
-   m_currentScene = sceneName;
+  m_sceneMap[sceneName] = scene;
+  m_currentScene = sceneName;
 
-   std::cout << "Changing scene to " << m_currentScene << std::endl;
+  std::cout << "Changing scene to " << m_currentScene << std::endl;
 }
 
 void GameEngine::run()
 {
-   std::cout << "Game engine running.\n";
+  std::cout << "Game engine running.\n";
 
-   sf::Clock clock;
-   //pause functionality here
-   //systems that run when paused (rendering)
-   //systems shouldnt (movement/input)
-
-   while(isRunning())
-   {
-      m_deltaTime = clock.restart().asSeconds();
-
+  while(isRunning())
+    {
       sUserInput();
       update();
       sRender();
-   }
+    }
 }
 
 void GameEngine::quit()
 {
-   m_running = false;
-   m_window.close();
+  m_running = false;
+  m_window.close();
 }
 
 sf::RenderWindow& GameEngine::window()
 {
-   return m_window;
+  return m_window;
 }
 
 std::shared_ptr<Scene> GameEngine::currentScene()
 {
-   auto it = m_sceneMap.find(m_currentScene);
-   if (it != m_sceneMap.end()) {
-      return it->second;
-   }
-   return nullptr;
+  auto it = m_sceneMap.find(m_currentScene);
+  if (it != m_sceneMap.end()) {
+    return it->second;
+  }
+  return nullptr;
 }
 
 
 bool GameEngine::isRunning()
 {
-   return m_running;
+  return m_running;
 }
