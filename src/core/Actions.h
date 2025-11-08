509d3d3661be04d75f5d02ac4dde0b639629c8b5
jonel <iamjonel7@gmail.com>
2025-11-09 03:20:27 +0800

    Fixed indentations

--- src/core/Actions.h
+++ src/core/Actions.h
@@ -1,17 +1,13 @@
 #pragma once
 
-#include <string>
-
-
-class Action{
-        std::string m_name = "none";
-        std::string m_type = "none"; //start or end (released)
+class Action
+{
+  std::string m_name = "none";
+  std::string m_type = "none"; //start or end (released)
 
 public:
-        Action();
-        Action(const std::string& name, const std::string& type);
+  Action(const std::string& name, const std::string& type);
 
-        const std::string& name() const;
-        const std::string& type() const;
-        std::string toString() const;
+  const std::string& name() const;
+  const std::string& type() const;
 };
