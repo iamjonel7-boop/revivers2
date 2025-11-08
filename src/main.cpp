a6370d8dd21e8196609b81dfa2d344c5c186bb7e
jonel <iamjonel7@gmail.com>
2025-11-09 00:53:52 +0800

    Restarted doing the project. Removed most of source files.

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "GrammarBicol.h"
-#include <iostream>
-
-int main()
-{
-        GameEngine revivers("assets/gameVerbs.json", "assets/gameNouns.json");
-        revivers.run();
-}
--- src/managers/ManagerSystem.cpp
+++
@@ -1,5 +0,0 @@
-#include "ManagerSystem.h"
-
-void SystemManager::registerSystem(const std::string& systemName, std::function<void(EntityManager&, const std::map<std::string, std::shared_ptr<Entity>>&, const std::string& aspect)> system)
-{
-t
