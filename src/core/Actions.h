b361f2895a8d87dfbab655b2ccbd3bb744eecd80
jonel <iamjonel7@gmail.com>
2025-11-16 06:20:41 +0800

    Fixed by adding include string

--- src/core/Actions.h
+++ src/core/Actions.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <string>
+
 class Action
 {
   std::string m_name = "none";
   std::string m_type = "none"; //start or end (released)
 
 public:
+  Action();
   Action(const std::string& name, const std::string& type);
 
   const std::string& name() const;
