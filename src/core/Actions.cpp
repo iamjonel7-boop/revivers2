a4077d1f6951747152441205f358464ae8c492bc
jonel <iamjonel7@gmail.com>
2025-11-17 03:03:27 +0800

    Resolved a warning of declaration and intialisation mismatch.

--- src/core/Actions.cpp
+++ src/core/Actions.cpp
@@ -3,14 +3,13 @@
 
 Action::Action()
   : m_type(ActionType::NONE) //START or END
-
   , m_name(-1) //moveLeft, right, up, etc
 {
 }
 
 Action::Action(int name, ActionType type)
-  : m_name(name)
-  , m_type(type)
+  : m_type(type)
+  , m_name(name)
 {
 }
 
