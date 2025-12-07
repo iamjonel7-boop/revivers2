445e75402075f35b1b8f78edf4e39afb9b6fed5d
jonel <iamjonel7@gmail.com>
2025-12-07 22:46:01 +0800

    feat: Make function table for updates.
    Less usage of switches

--- src/scenes/SceneMain.h
+++ src/scenes/SceneMain.h
@@ -15,13 +15,25 @@ protected:
 				MOVE_LEFT,
 				MOVE_RIGHT,
 				SELECT_TILE,
-				OPEN_INVENTORY
+				OPEN_INVENTORY,
+				SELECT_ENTITY,
+				RETURN
 		};
 
 		enum class MapControlState
 		{
 				NAVIGATING,
 				SENTENCING,
+				COUNT
+		};
+
+		void updateNavigating(std::string& help1, std::string& help2);
+		void updateSentencing(std::string& help1, std::string& help2);
+
+		using updateStateFunc = void(SceneMain::*)(std::string& help1, std::string& help2);
+		static constexpr std::array<updateStateFunc, (size_t)MapControlState::COUNT> m_updateTable = {
+				&SceneMain::updateNavigating,
+				&SceneMain::updateSentencing
 		};
 
 		sf::View m_mainView;
@@ -40,6 +52,7 @@ protected:
 
 		void handleCursorNavigation(const Action& action, ActionName act, CInput& cinput);
 		void handleSentencing(ActionName act);
+		void updateCursorPos();
 
 		void makeMapView();
 		void makeMapGrid();
@@ -47,12 +60,19 @@ protected:
 		void makeBuildings();
 		void makePaths();
 
-		void updateCursorPos();
 
 		void renderTileGrid();
 		void renderCursor();
 		void renderBuildings();
 		void renderPaths();
+
+		sf::Font m_font;
+
+		sf::Text m_helpMsg;
+		sf::Text m_helpMsg2;
+
+		sf::RectangleShape m_sentencePanel;
+		sf::RectangleShape m_sentencingPanel;
 
 public:
 		SceneMain(GameEngine* gameEngine);
